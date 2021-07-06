4f9951852dc1cff0ada239dd43118ce7a420ccd9
MattWeinberg24 <mattweinberg@wustl.edu>
2021-07-06 12:40:16 -0400

    hai iie

--- testing.js
+++ testing.js
@@ -1,14 +1,38 @@
+var currentWord;
+
+$(document).ready(function(){
+    randomKanaNominal();
+});
+
+function confirmAnswer(){
+    var answer = document.getElementById("inp").value;
+    if (answer != ""){
+        if (answer == currentWord.english){
+            document.getElementById("result").innerHTML = "はい";
+        }
+        else {
+            document.getElementById("result").innerHTML = "いいえ";
+        }
+        setTimeout(function(){
+            randomKanaNominal();
+        },1000);
+    }
+}
+
+function randomKanaNominal(){
+    document.getElementById("inp").value = "";
+    document.getElementById("result").innerHTML = "";
+    useJSONFromFile("nominals.json", function(nominals){
+        var num = Math.floor(Math.random() * nominals.length);
+        currentWord = nominals[num];
+        document.getElementById("word").innerHTML = currentWord.kana;
+    });
+}
+
 function useJSONFromFile(filename, callback){
     $(document).ready(function(){
         $.getJSON(filename, function(data){
             callback(data);
         })
     })
-}
-
-function randomEnglishNominal(){
-    useJSONFromFile("nominals.json", function(nominals){
-        var num = Math.floor(Math.random() * nominals.length);
-        document.getElementById("word").innerHTML = nominals[num].english;
-    });
 }
