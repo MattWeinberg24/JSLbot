3abc7190cc3a81ab229b12068509015d35624bfe
MattWeinberg24 <mattweinberg@wustl.edu>
2021-07-13 16:14:05 -0400

    red and green

--- vocab.js
+++ vocab.js
@@ -1,9 +1,11 @@
 var currentWord; //holds the current word
+var currentWordIndex; //holds the current word index
 var answered = false; //true when user has answered and is viewing the result, false otherwise
 
 //runs when website is loaded
 $(document).ready(function(){
     randomKanaNominal();
+    renderTable();
     $("#inp").on("submit", function(e) {
         e.preventDefault();
         if(answered){
@@ -25,9 +27,11 @@ function confirmAnswer(){
     if(answer != ""){
         if (correct(answer)){
             document.getElementById("result").innerHTML = "はい";
+            document.getElementById("vocab-list").rows[currentWordIndex + 1].style.backgroundColor = "green";
         }
         else {
             document.getElementById("result").innerHTML = "いいえ";
+            document.getElementById("vocab-list").rows[currentWordIndex + 1].style.backgroundColor = "red";
         }
     }
 }
@@ -38,8 +42,8 @@ function correct(answer){
         document.getElementById("correct-answers").innerHTML = capitalize(currentWord.english[0]);
         return currentWord.english.includes(answer);
     }
-    else{
-        var message = currentWord.romazi + ", " + currentWord.kana + (currentWord.kana == currentWord.kanzi ? "" : ", " + currentWord.kanzi);
+    else{                                                                   //eventually may need updating
+        var message = currentWord.romazi + ", " + currentWord.kana + (currentWord.kana.includes(currentWord.kanzi[0]) ? "" : ", " + currentWord.kanzi);
         document.getElementById("correct-answers").innerHTML = message;
         return answer == currentWord.romazi || answer == currentWord.kana || answer == currentWord.kanzi;
     }
@@ -51,9 +55,9 @@ function randomKanaNominal(){
     document.getElementById("result").innerHTML = "";
     document.getElementById("correct-answers").innerHTML = "";
 
-    $.getJSON("words/nominals.json", function(nominals){
-        var num = Math.floor(Math.random() * nominals.length);
-        currentWord = nominals[num]; //change num to manually select word
+    $.getJSON("words.json", function(words){
+        currentWordIndex = Math.floor(Math.random() * words.nominals.length);
+        currentWord = words.nominals[currentWordIndex]; //change num to manually select word
         if(document.getElementById("english-option-q").checked){
             document.getElementById("word").innerHTML = capitalize(currentWord.english[0]);
         }
@@ -67,6 +71,26 @@ function randomKanaNominal(){
             document.getElementById("word").innerHTML = currentWord.kanzi;
         }
     });
+}
+
+function renderTable(){
+    var table = document.getElementById("vocab-list");
+    $.getJSON("words.json", function(words){
+        words.nominals.forEach(function(nominal){
+            var row = table.insertRow();
+            var cell1 = row.insertCell();
+            cell1.innerHTML = nominal.kanzi[0];
+            var cell2 = row.insertCell();
+            cell2.innerHTML = nominal.kana[0];
+            var cell3 = row.insertCell();
+            cell3.innerHTML = nominal.romazi[0];
+            var cell4 = row.insertCell();
+            cell4.innerHTML = capitalize(nominal.english[0]);
+            var cell5 = row.insertCell();
+            //cell5.innerHTML = nominal.lesson;
+        });
+    });
+    
 }
 
 //capitalizes first letter of str
