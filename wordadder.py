42b4fa3f9738648fb4b27d4c8d83b974a866d7a1
MattWeinberg24 <mattweinberg@wustl.edu>
2021-08-15 15:49:06 -0400

    added quotations around new words

--- wordadder.py
+++ wordadder.py
@@ -1,20 +1,24 @@
 import os
 
 path = "newwords.txt" # path of the textfile
-categories = ["japanese","english","category","lesson"] # required json properties
-optional_categories = ["politeness","question"] # optional json properties
+categories = [("\"japanese\"", True),("\"english\"", True),("\"category\"", False), ("\"lesson\"", False), ("\"politeness", False), ("\"question\"", False)] # required json properties
+optional_categories = ["\"politeness","\"question\""] # optional json properties
 
-with open(path, 'w') as f:
+with open(path, 'w', encoding='utf8') as f:
     nextword = True
     while nextword:
         f.write("{")
         for c in categories:
-            p = input(c + ": ")
-            f.write("\n\t" + c + ": " + p)
-        for c in optional_categories:
-            p = input(c + ": ")
-            if (p != ""):
-                f.write("\n\t" + c + ": " + p)
+            s = input(c[0] + ": ").strip()
+            if (s != ""):
+                l = s.split(",")
+                p = ""
+                for word in l:
+                    p += "\"" + word.strip() + "\", "
+                p = p.strip(", ")
+                if (c[1]):
+                    p = "[" + p + "]"
+                f.write("\n\t" + c[0] + ": " + p)
         f.write("\n},\n")
         nextword = input() == ""
     
