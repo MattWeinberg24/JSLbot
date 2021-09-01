2afafa33ea0e4b98e274f0cdd69b5d7a4315b898
MattWeinberg24 <mattweinberg@wustl.edu>
2021-08-31 22:33:27 -0500

    added support for other parts of speech

--- wordadder.py
+++ wordadder.py
@@ -1,31 +1,35 @@
 import os
 
 path = "newwords.txt" # path of the textfile
-categories = [("\"japanese\"", True),("\"english\"", True),("\"category\"", False), ("\"lesson\"", False)] # json properties. second value determines if list or not.
+
+# json properties. second value determines if list or not.
+properties = [("\"japanese\"", True),("\"english\"", True), ("\"lesson\"", False)]
+nominal_properties = [("\"category\"", False)]
+verbal_properties = [("\"ru\"", False),("\"operational\"", False)]
+
+current_properties = properties # USAGE: CONCATENATE ANY ADDITIONAL PROPERTIES YOU WANT
 
 with open(path, 'w', encoding='utf8') as f:
     nextword = True
     while nextword:
         f.write("{")
-        for i,c in enumerate(categories):
-            s = input(c[0] + ": ").strip()
+        for i,p in enumerate(current_properties):
+            s = input(p[0] + ": ").strip()
             if (s != ""): # only add json if something was inputted
                 if (i > 0): # don't add comma before the first property
                     f.write(",")
                 if (s != "true"): # only add quotes around values if not supposed to be boolean true
                     l = s.split(",")
-                    p = ""
+                    text = ""
                     for word in l:
-                        p += "\"" + word.strip() + "\", "
-                    p = p.strip(", ")
-                    if (c[1]): 
-                        p = "[" + p + "]"
+                        text += "\"" + word.strip() + "\", "
+                    text = text.strip(", ")
+                    if (p[1]): 
+                        text = "[" + text + "]"
                 else:
-                    p = s
-                f.write("\n\t" + c[0] + ": " + p)
+                    text = s
+                f.write("\n\t" + p[0] + ": " + text)
         f.write("\n},\n")
         nextword = input() == ""
-    
-    
 
 os.system(path) # opens the textfile
