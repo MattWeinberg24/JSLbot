de12143ff8080ef68350ca1c409ed799a17eb4c5
MattWeinberg24 <mattweinberg@wustl.edu>
2021-09-26 17:22:17 -0500

    moved to new folder

--- data/words.js
+++ data/words.js
@@ -1381,15 +1381,15 @@ var nominals = [
     },
     {
         "japanese": ["研究所"],
-            "english": ["research institute"],
-            "lesson": "13B",
-            "category": "place"
+        "english": ["research institute"],
+        "lesson": "13B",
+        "category": "place"
     },
     {
-            "japanese": ["実"],
-            "english": ["truth","reality"],
-            "lesson": "13B",
-            "category": "quality"
+        "japanese": ["実"],
+        "english": ["truth","reality"],
+        "lesson": "13B",
+        "category": "quality"
     }
 ]
 
